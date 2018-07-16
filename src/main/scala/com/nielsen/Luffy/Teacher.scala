42bb5cf5fece14374b29b7b26300b847f7206fcb
lulu7001 <Luffy.Lu@nielsen.com>
2018-07-16 16:56:25 +0800

    add Luffy's demo

---
+++ src/main/scala/com/nielsen/Luffy/Teacher.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by lulu7001 on 2018/7/12.
+  */
+
+class Teacher(var name:String,var sex: String) {
+
+//  def name_=(newName:String): Unit ={
+//    this.name = newName;
+//  }
+}
