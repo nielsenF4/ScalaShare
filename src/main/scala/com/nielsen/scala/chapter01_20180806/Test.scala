29486c3a02b55c1d5999543ad3c7d1a85b3a735c
shhu6001 <hui.shen@nielsen.com>
2018-08-06 18:16:09 +0800

    chapter01 share

--- src/main/scala/com/nielsen/scala/chapter01_20180806/Test.scala
+++ src/main/scala/com/nielsen/scala/chapter01_20180806/Test.scala
@@ -5,37 +5,48 @@ import scala.collection.immutable.StringOps
 object Test extends App {
 
   //把函数作为实参传递给另外一个函数
-  //  def f(name: String) = {
-  //    "Hello, " + name
-  //  }
+  //    def f(name: String) = {
+  //      "Hello, " + name
+  //    }
   //
-  //  def hello(f: (String) => String, name: String) = {
-  //    f(name)
-  //  }
+  //    def hello(f: (String) => String, name: String) = {
+  //      f(name)
+  //    }
   //
-  //  println(hello(f, "clshen"))
+  //    println(hello(f, "clshen"))
 
   //把函数作为返回值
-  //  def sum(f: Int => Int): (Int, Int) => Int = {
-  //    def sumF(a: Int, b: Int): Int = {
-  //      if (a > b) 0
-  //      else f(a) + sumF(a + 1, b)
+  //    def sum(f: Int => Int): (Int, Int) => Int = {
+  //     def sumF(a: Int, b: Int): Int = {
+  //        if (a > b) 0
+  //        else f(a) + sumF(a + 1, b)
+  //      }
+  //      sumF
   //    }
-  //    sumF
-  //  }
 
   //把函数赋值给变量
-  //  val hiData = hiBigData _
-  //  def hiBigData(name: String) {
-  //    println("Hi, " + name)
-  //  }
-  //  hiData("BigData")
+      val hiData = hiBigData _
+      def hiBigData(name: String) {
+        println("Hi, " + name)
+      }
+      hiData("BigData")
 
   //把函数存储在数据结构里
-  val fArray = Array((x: Int, y: Int) => StringOps, (x: Int, y: Int, z: Int) => Int, (x: Int, y: Int, z: Int) => Unit)
-  
-  fArray.foreach(p => println(p))
-  
+  //  val fArray = Array((x: Int, y: Int) => StringOps, (x: Int, y: Int, z: Int) => Int, (x: Int, y: Int, z: Int) => Unit)
+  //
+  //  fArray.foreach(p => println(p))
+
+  def test(x: Int, y: Int): String = {
+    ""
+  }
+
+  def test1(x: Int, y: Int): String = {
+    ""
+  }
+
+  val f = Array(test(_, _), test1(_, _))
+
+  f.foreach(p => println(p))
 
 }
 
